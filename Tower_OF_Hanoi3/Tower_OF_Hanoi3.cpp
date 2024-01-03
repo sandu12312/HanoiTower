465f9b4a76d406983bf85a4139b9ecd6bce61b76
Gheorghita Alexandru <55880052+sandu12312@users.noreply.github.com>
2024-01-03 20:10:01 +0200

    DiskMoves
    This functionality makes the game movment of the disks

--- Tower_OF_Hanoi3/Tower_OF_Hanoi3.cpp
+++ Tower_OF_Hanoi3/Tower_OF_Hanoi3.cpp
@@ -9,20 +9,143 @@ using namespace std;
 
 
 int disk_1[10], disk_2[10], disk_3[10];
+int disk_number;
+int ok = 0;
+int i1, i2, i3;
 
-asdasd
+
+void change_disk(int x, int y)
+{
+    
+    if (ok == 0)
+    {
+         i1 = disk_number, i2 = 0, i3 = 0;
+        ok = 1;
+    }
+    disk_2[0] = 99;
+    disk_3[0] = 99;
+       if (x == 1 && y == 2)
+        {
+            if (disk_2[i2] > disk_1[i1])
+            {
+                disk_2[i2+1] = disk_1[i1];
+                disk_1[i1] = 0;
+                i2++;
+                i1--;
+            }
+            else
+            {
+                cout << "Invalid move"<<"\n\n";
+            }
+        }
+       else if (x==1 && y==3)
+       {
+           if (disk_3[i3] > disk_1[i1])
+           {
+               disk_3[i3 + 1] = disk_1[i1];
+               disk_1[i1] = 0;
+               i3++;
+               i1--;
+           }
+           else
+           {
+               cout << "Invalid move" << "\n\n";
+           }
+
+       }
+       else if (x == 2 && y == 3)
+       {
+           if (disk_3[i3] > disk_2[i2])
+           {
+               disk_3[i3 + 1] = disk_2[i2];
+               disk_2[i2] = 0;
+               i3++;
+               i2--;
+           }
+           else
+           {
+               cout << "Invalid move" << "\n\n";
+           }
+
+       }
+       else if (x == 2 && y == 1)
+       {
+           if (disk_1[i1] > disk_2[i2])
+           {
+               disk_1[i1 + 1] = disk_2[i2];
+               disk_2[i2] = 0;
+               i1++;
+               i2--;
+           }
+           else
+           {
+               cout << "Invalid move" << "\n\n";
+           }
+
+       }
+       else if (x == 3 && y == 1)
+       {
+           if (disk_1[i1] > disk_3[i3])
+           {
+               disk_1[i1 + 1] = disk_3[i3];
+               disk_3[i3] = 0;
+               i1++;
+               i3--;
+           }
+           else
+           {
+               cout << "Invalid move" << "\n\n";
+           }
+
+       }
+       else if (x == 3 && y == 2)
+       {
+           if (disk_2[i2] > disk_3[i3])
+           {
+               disk_2[i2 + 1] = disk_3[i3];
+               disk_3[i3] = 0;
+               i2++;
+               i3--;
+           }
+           else
+           {
+               cout << "Invalid move" << "\n\n";
+           }
+
+       }
+}
+
 
 int main()
 {
 
 
     int i;
+    cout << "Cititi numarul de disk - uri:";
+    cin >> disk_number;
+    Disk_Initialization(disk_number);
+   // Display_Towers(disk_number);
+    change_disk(1, 2);
+    cout << "\n";
+    Display_Towers(disk_number);
+    change_disk(1, 3);
+    cout << "\n";
+    Display_Towers(disk_number);
+    change_disk(2, 3);
+    cout << "\n";
+    Display_Towers(disk_number);
 
-    Disk_Initialization(8);
-    Display_Towers(8);
+
+
+  
+ 
+    int opt;
+    
+    
 
     for (i = 1; i <= 8; i++)
     {
+       
         cout<<disk_2[i]<<"\n";
 
     }
