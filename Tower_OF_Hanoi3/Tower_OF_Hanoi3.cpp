774cf7112e418c476b9c3cb726c317b1b452f759
Gheorghita Alexandru <55880052+sandu12312@users.noreply.github.com>
2024-01-04 22:22:59 +0200

    Trying the auto mode for the game

--- Tower_OF_Hanoi3/Tower_OF_Hanoi3.cpp
+++ Tower_OF_Hanoi3/Tower_OF_Hanoi3.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 #include "Tower_Of_Hanoi.h"
-
+#include "math.h"
 
 using namespace std;
 
@@ -10,9 +10,85 @@ using namespace std;
 
 int disk_1[10], disk_2[10], disk_3[10];
 int disk_number;
+int game_over = 0, steps_number;
+
+int j_first, j2, j3;
+int k;
+
+void auto_solve(int number_of_discs)
+{
+    steps_number = pow(2, number_of_discs) - 1;
+     k = 1;
+     j_first = number_of_discs;
+     j2 = 0;
+     j3 = 0;
+     disk_1[0] = 99;
+     disk_2[0] = 99;
+     disk_3[0] = 99;
+    while (k <= steps_number)
+    {
+        if ((k % 3) == 1)
+        {
+            if (disk_3[j3] > disk_1[j_first]) {
+                disk_3[j3++] = disk_1[j_first];
+                j3++;
+                j_first--;
+                k++;
+                Display_Towers(number_of_discs);
+            }
+            else
+            {
+                disk_1[j_first++] = disk_3[j3];
+                j_first++;
+                j3--;
+                k++;
+                Display_Towers(number_of_discs);
+            }
+        }
+        if ((k % 3) == 2)
+        {
+            if (disk_2[j2] > disk_1[j_first])
+            {
+                disk_2[j2++] = disk_1[j_first];
+                j2++;
+                j_first--;
+                k++;
+                Display_Towers(number_of_discs);
+            }
+            else
+            {
+                disk_1[j_first++] = disk_2[j2];
+                j_first++;
+                j2--;
+                k++;
+                Display_Towers(number_of_discs);
+            }
+        
+        }
+
+        if ((k % 3) == 0)
+        {
+            if (disk_2[j2] > disk_3[j3])
+            {
+                disk_2[j2++] = disk_3[j3];
+                j2++;
+                j3--;
+                k++;
+                Display_Towers(number_of_discs);
+            }
+            else
+            {
+                disk_3[j3++] = disk_2[j2];
+                j3++;
+                j2--;
+                k++;
+                Display_Towers(number_of_discs);
+            }
+        }
 
 
-int game_over = 0;
+    }
+}
 
 
 int main()
@@ -82,6 +158,13 @@ int main()
 
            
             break;
+        case 3:
+            cout << "\nRead the number of disks the bot to play:";
+            cin >> disk_number;
+            Disk_Initialization(disk_number);
+            auto_solve(disk_number);
+            break;
+
         default:
             cout << "--------------------\n";
             cout << "Optiune invalida"<<"\n";
